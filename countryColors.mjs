a1ca0c4ab7c410c55ab14154103972b24dfa37ca
robert <kaukana0@tuta.io>
2022-09-15 14:52:49 +0200

    initial commit

---
+++ countryColors.mjs
@@ -0,0 +1,38 @@
+export function process(inputData, output) {
+    output.colorPalette = [
+        "#069F73",
+        "#CC79A7",
+        "#5EB3E5",
+        "#D36027",
+        "#000000",
+        "#0773B3",
+        "#8214A0",
+        "#0AB45A",
+        "#00A0FA",
+        "#AA0A3C",
+        "#006E82",
+        "#FA78FA",
+        "#14D2DC",
+        "#005AC8",
+        "#FA7850",
+        "#FA5D7D",
+        "#A05FBE",
+        "#55550B",
+        "#46AA96",
+        "#501937",
+        "#959595",
+        "#DE2D26",
+        "#146964",
+        "#C80F9B",
+        "#005B78",
+        "#636363",
+        "#64AA2D",
+        "#192D23", 
+        "#9C9CD2", 
+        "#5FB996"
+    ]
+
+    output.countryColors = {
+        EU: "#0E47CB",
+    }
+}
